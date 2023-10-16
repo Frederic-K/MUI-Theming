e7f8e717465e4a35e55829e173e65edc134afc3f
frederic.k <frederic.kreuder.pro@gmail.com>
2023-10-16 11:35:42 +0200

    testing dark mode

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,9 +1,32 @@
-import { blue, pink } from '@mui/material/colors'
-import { createTheme } from '@mui/material/styles'
+import { amber, deepOrange, grey } from '@mui/material/colors'
 
-export const appTheme = createTheme({
+const getDesignTokens = (mode) => ({
   palette: {
-    primary: blue,
-    secondary: pink,
+    mode,
+    primary: {
+      ...amber,
+      ...(mode === 'dark' && {
+        main: amber[300],
+      }),
+    },
+    ...(mode === 'dark' && {
+      background: {
+        default: deepOrange[900],
+        paper: deepOrange[900],
+      },
+    }),
+    text: {
+      ...(mode === 'light'
+        ? {
+            primary: grey[900],
+            secondary: grey[800],
+          }
+        : {
+            primary: '#fff',
+            secondary: grey[500],
+          }),
+    },
   },
 })
+
+export default getDesignTokens
