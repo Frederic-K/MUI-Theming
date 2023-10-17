978f84514341e2c863d27be0e4e8339a7ebd7e3c
frederic.k <frederic.kreuder.pro@gmail.com>
2023-10-17 09:13:19 +0200

    Upgrade dark mode theme

--- src/themes/ToggleColorMode.js
+++ src/themes/ToggleColorMode.js
@@ -1,6 +1,7 @@
 import { useState, useMemo } from 'react'
 import { createTheme } from '@mui/material/styles'
 import getDesignTokens from './Theme'
+import { teal } from '@mui/material/colors'
 
 export default function ToggleColorMode() {
   const [mode, setMode] = useState('light')
@@ -12,7 +13,7 @@ export default function ToggleColorMode() {
     }),
     []
   )
-
+  // Deffault dark mode
   // const currentTheme = useMemo(
   //   () =>
   //     createTheme({
@@ -23,7 +24,28 @@ export default function ToggleColorMode() {
   //   [mode]
   // )
 
-  const currentTheme = useMemo(() => createTheme(getDesignTokens(mode)), [mode])
+  // Upgrade default dark mode
+  const currentTheme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+          primary: {
+            ...teal,
+            ...(mode === 'dark' && {
+              main: teal[300],
+            }),
+          },
+          secondary: {
+            main: '#f5f5f5',
+          },
+        },
+      }),
+    [mode]
+  )
+
+  // Full custom dark mode
+  // const currentTheme = useMemo(() => createTheme(getDesignTokens(mode)), [mode])
 
   return {
     theme: currentTheme,
